a5e9b22828e40930ea66faa23e86e70e62a3e449
Christian Autermann <c.autermann@52north.org>
2020-03-17 15:34:42 +0100

    support for setting SOS credentials in docker

--- sos/sos-entrypoint.sh
+++ sos/sos-entrypoint.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+set_admin_user() {
+  local username="$1"
+  local password="$2"
+  local temp="$(mktemp)"
+  password=$(echo "${password}" | bcrypt --rounds 10)
+  jq --arg username "${username}" --arg password "${password}" \
+    '.users = {"\($username)": $password}' \
+    "${FAROE_CONFIGURATION}" > "${temp}"
+  mv -f "${temp}" "${FAROE_CONFIGURATION}"
+
+}
+
+if [ -n "${SOS_ADMIN_USERNAME}" -a -n "${SOS_ADMIN_PASSWORD}" ]; then
+  set_admin_user "${SOS_ADMIN_USERNAME}" "${SOS_ADMIN_PASSWORD}"
+fi
+
+
 if [ "${SOS_DATASOURCE_TYPE}" = "postgres" ]; then
   : ${SOS_DATASOURCE_USERNAME:=postgres}
   : ${SOS_DATASOURCE_PASSWORD:=postgres}
